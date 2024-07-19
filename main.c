d239ac46005c35031c462005c395f8cee6db3735
yeeben <155392286+yeeben@users.noreply.github.com>
2024-07-19 12:33:04 -0700

    Dataset gitignore

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
 #include "stdio.h"
+#include <stdint.h>
+
+#define HEIGHT_IMAGE 28
+#define WIDTH_IMAGE 28
+#define OUTPUT_LAYER_SIZE 10
+
+static uint32_t image[HEIGHT_IMAGE][WIDTH_IMAGE] = {};
 
 
 int main() {
